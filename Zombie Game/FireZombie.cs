c082ad10335820966d1c18272fab9340420c5f6e
yuvalelarat <102436759+yuvalelarat@users.noreply.github.com>
2023-05-25 16:09:21 +0300

    Add files via upload

---
+++ Zombie Game/FireZombie.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zombie_Game
+{
+    class FireZombie : Zombie
+    {
+        Random randNum = new Random();
+        private PictureBox attack = new PictureBox();
+        private Timer attackTimer = new Timer();
+
+
+        public FireZombie()
+        {
+            health = 150;
+            speed = 4;
+            damage = 1;
+        }
+        public override PictureBox Spawn()
+        {
+            PictureBox fireZombie = new PictureBox();
+            fireZombie.Tag = "fireZombie";
+            fireZombie.Image = Properties.Resources.FireZombieLeft;
+            fireZombie.Left = randNum.Next(0, 1000);
+            fireZombie.Top = randNum.Next(0, 1000);
+            fireZombie.SizeMode = PictureBoxSizeMode.AutoSize;
+            FireZombieList.Add(fireZombie);
+            return fireZombie;
+        }
+        public override void resetHealth()
+        {
+            health = 150;
+        }
+        public override void resetSpeed()
+        {
+            speed = 5;
+        }
+        public override void MakeAttack(Form form)
+        {
+            PictureBox trap = new PictureBox();
+            trap.Image = Properties.Resources.DeathStep;
+            trap.SizeMode = PictureBoxSizeMode.AutoSize;
+            trap.Left = randNum.Next(10, 950 - trap.Width);
+            trap.Top = randNum.Next(60, form.ClientSize.Height - trap.Height);
+            trap.Tag = "trap";
+            form.Controls.Add(trap);
+            trap.BringToFront();
+        }
+    }
+}
